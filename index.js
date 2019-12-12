85ebb2076ee8d4b887c60931a530c95741fdcaf1
Sarah Alafif <sar1997yah@gmail.com>
2019-12-12 19:08:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,6 @@ BoardMember.prototype.doCharity=function(){
 BoardMember.prototype.releasePressStatement=function(){
   return "You will see great things from Scuber."
 }
+BoardMember.prototype.releasePressStatement=function(){
+  return "You will see great things from Scuber."
+}
