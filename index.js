1c132b1276789085282ffab365fa9bee13814494
Sarah Alafif <sar1997yah@gmail.com>
2019-12-12 19:07:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,6 @@ BoardMember.prototype.veto=function(){
 BoardMember.prototype.approve=function(){
   return "You can do that!"
 }
+BoardMember.prototype.approve=function(){
+  return "You can do that!"
+}
